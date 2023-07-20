55797c9914ba3f213d74636df7e93ce23b1cc946
Jacob Hepkema <jacobhepkema@gmail.com>
2023-07-20 14:49:06 +0100

    this should work now

--- untar_files.sh
+++ untar_files.sh
@@ -4,3 +4,10 @@ tar -xzvf data/humankidney/20230125_kidney_repr_aligned_motif_patterns_in_genes.
 tar -xzvf data/tm/20221111_tm_all_loo_scores_df.csv.tar.gz
 tar -xzvf data/tm/20230126_tm_repr_aligned_motif_patterns_in_genes.csv.tar.gz
 tar -xzvf data/humanbrain/20230110_trevino_100neighbours_all_loo_scores_df.csv.tar.gz
+
+mv 20230123_kidney_all_loo_scores_df.csv data/humankidney/
+mv 20230125_kidney_repr_aligned_motif_patterns_in_genes.csv data/humankidney/
+mv 20221111_tm_all_loo_scores_df.csv data/tm/
+mv 20230126_tm_repr_aligned_motif_patterns_in_genes.csv data/tm/
+mv 20230110_trevino_100neighbours_all_loo_scores_df.csv data/humanbrain/
+
